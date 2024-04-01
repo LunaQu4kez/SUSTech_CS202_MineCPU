b87b02adcbe3830451fbc5261048c13380a52338
Krurmi33 <2397429787@qq.com>
2024-04-02 01:49:31 +0800

    update: simplify Stage_MEM.sv

--- sources/core/Stage_MEM.sv
+++ sources/core/Stage_MEM.sv
@@ -6,12 +6,9 @@ module Stage_MEM (
     input  logic [`DATA_WID    ] write_addr,
     input  logic [`DATA_WID    ] write_data,
     input  logic [`REGS_WID    ] EX_MEM_rd,
-	output logic [`REGS_WID    ] EX_MEM_rd_out,
     output logic [`REGS_WID    ] EX_rd_out,
-    output logic [`DATA_WID    ] EX_MEM_data_out,
     output logic [`DATA_WID    ] addr_out,
     output logic [`DATA_WID    ] data_out,
-    output logic              	 EX_MEM_RegWrite_out,
     output logic [`WB_CTRL_WID ] WB_ctrl_out,
     // interact with Memory
     output logic [`DATA_WID    ] mem_addr,
@@ -22,12 +19,9 @@ module Stage_MEM (
 );
 
     assign WB_ctrl_out = WB_ctrl_in;
-    assign EX_MEM_rd_out = EX_MEM_rd;
     assign EX_rd_out = EX_MEM_rd;
-    assign EX_MEM_data_out = write_addr;
     assign addr_out = write_addr;
     assign data_out = mem_data;
-    assign EX_MEM_RegWrite_out = WB_ctrl_in[1];
 
     assign mem_addr = write_addr;
     assign mem_write_data = write_data;
