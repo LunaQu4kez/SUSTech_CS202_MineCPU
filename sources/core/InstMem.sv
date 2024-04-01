0031e3a2d47387c5ffb7c3ece98b092a6bfb0275
Krurmi33 <2397429787@qq.com>
2024-04-01 15:01:30 +0800

    update: Stage_Mem.sv

--- sources/core/InstMem.sv
+++ sources/core/InstMem.sv
@@ -1,13 +1,11 @@
+`include "Const.svh"
+
 module InstMem (
     input              clk,
     input  [`DATA_WID] addr,
     output [`DATA_WID] inst
 );
 
-    progrom instmem (
-        .clka(~clk),
-        .addra(addr[15:2]),
-        .douta(inst)
-    );
+    
     
 endmodule
---
+++ sources/core/Memory.sv
@@ -0,0 +1,10 @@
+`include "Const.svh"
+
+module Memory (
+    
+);
+
+
+
+
+endmodule
