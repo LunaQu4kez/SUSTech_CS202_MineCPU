7f3a8766f26bec41fb6fa1406be3ca448a22d7a4
Krurmi33 <2397429787@qq.com>
2024-04-02 15:14:31 +0800

    update: merge pc choose MUX into PC register

--- sources/core/PC.sv
+++ sources/core/PC.sv
@@ -1,15 +1,19 @@
+`include "Const.svh"
+
 module PC (
-    input              clk, rst,  // toDo: implement reset
-    input              PC_Write,  // stall, 1 yes, 0 no
-    input  [`DATA_WID] pc_in,
-    output [`DATA_WID] pc_out
+    input                   clk, rst,       // toDo: implement reset
+    input                   PC_Write,       // data adventure stall, 1 yes, 0 no
+    input  [`DATA_WID]      pc_branch_n,
+    input  [`DATA_WID]      pc_branch_y,
+    input                   branch,         // 1 jump branch, 0 do not jump(pc = pc + 4)
+    output [`DATA_WID]      pc_out
 );
 
     reg [31:0] pc = 0;
     assign pc_out = pc;
 
     always @(posedge clk) begin
-        pc <= (PC_Write == 1'b0) ? pc_in : pc;
+        pc <= (PC_Write == 1'b0) ? (branch == 1'b0 ? pc_branch_n : pc_branch_y) : pc;
     end
     
 endmodule
