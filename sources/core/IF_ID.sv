b44063818bda77d431b5a3f8eeb0ff7385c6744d
Krurmi33 <2397429787@qq.com>
2024-04-02 15:13:52 +0800

    update: add IF_Flush (for control adventure) wire to IF_ID

--- sources/core/IF_ID.sv
+++ sources/core/IF_ID.sv
@@ -1,8 +1,11 @@
+`include "Const.svh"
+
 module IF_ID (
-    input               clk, rst,
-    input               IF_ID_Write,        // stall, 1 yes, 0 no
-    input [31:0]        inst_in, pc_in,
-    output [31:0]       inst_out, pc_out
+    input                   clk, rst,
+    input                   IF_ID_Write,        // data adventure stall, 1 yes, 0 no
+    input                   IF_Flush,           // control adventure flush, 1 yes, 0 no
+    input  [`DATA_WID]      inst_in, pc_in,
+    output [`DATA_WID]      inst_out, pc_out
 );
 
     reg [31:0] inst = 0;
@@ -11,10 +14,8 @@ module IF_ID (
     assign pc_out = pc;
 
     always @(posedge clk) begin
-        inst <= (IF_ID_Write == 1'b0) ? inst_in : inst;
-        pc <= (IF_ID_Write == 1'b0) ? pc_in : pc;
+        inst <= (IF_ID_Write == 1'b0 & IF_Flush == 1'b0) ? inst_in : inst;
+        pc <= (IF_ID_Write == 1'b0 & IF_Flush == 1'b0) ? pc_in : pc;
     end
 
-
-    
 endmodule
