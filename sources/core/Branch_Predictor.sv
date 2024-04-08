21f3baa61d9b5106419a4ebd263c67b4a8e8bd88
chanbengz <chanbengz@foxmail.com>
2024-04-09 01:49:33 +0800

    update: finish Branch Predictor

--- sources/core/Branch_Predictor.sv
+++ sources/core/Branch_Predictor.sv
@@ -4,7 +4,7 @@ module Branch_Predictor (
     input  logic             clk, rst,
     // whether to branch and predict, from Control
     input  logic             branch, predict,
-    // process jalr
+    // process jalr, ujtype indicates jal
     input  logic [`DATA_WID] rs1_data,
     input  logic             ujtype,
     // pc is from IF, imm is from ID, old_pc is from EX
@@ -14,6 +14,7 @@ module Branch_Predictor (
     output logic [`DATA_WID] target_pc,
     output logic             predict_result, predict_fail
 );
+
     reg [1:0] History_Table [0: (1 << 12) - 1];
     initial begin
         for (int i = 0; i < (1 << 12); i = i + 1) begin
@@ -21,18 +22,54 @@ module Branch_Predictor (
         end
     end
 
-    logic [11:0] table_addr;
+    logic [11:0] table_addr; // use pc[13:2] as index since last 2 bits are always 0
     assign table_addr = pc[13:2];
     assign predict_fail = old_predict != old_actual;
 
-    always_comb begin // 0: strongly not taken, 1: weakly not taken, 2: weakly taken, 3: strongly taken
-        predict_result = History_Table[table_addr] >= 2'b10;
-        // target_pc = predict_result ? pc + imm : pc + 4;
-        target_pc = pc + 4;
+    always_comb begin : Predict // 0: strongly not taken, 1: weakly not taken, 2: weakly taken, 3: strongly taken
+        if (predict_fail | rst) begin
+            predict_result = old_actual;
+            target_pc = old_pc;
+        end else begin
+            unique case ({branch, predict})
+                2'b00: begin // no branch, no predict: continue
+                    predict_result = 1'b0;
+                    target_pc = pc + 4;
+                end
+                2'b10: begin // branch, no predict: jump
+                    predict_result = 1'b1;
+                    if (ujtype) target_pc = predict_result ? pc + imm : pc + 4; // jal
+                    else target_pc = predict_result ? rs1_data + imm : pc + 4; // jalr
+                end
+                2'b11: begin // branch, predict: beq, bne, blt, bge, bltu, bgeu
+                    predict_result = History_Table[table_addr] > 2'b01;
+                    target_pc = pc + imm;
+                end
+                default: begin // dont care case: continue
+                    predict_result = 1'b0;
+                    target_pc = pc + 4;
+                end
+            endcase
+        end
     end
 
     always_ff @(posedge clk) begin : Update_Table
-        
+        if (old_branch) begin // update table
+            if (old_actual) begin
+                if (History_Table[table_addr] < 2'b11) begin
+                    History_Table[table_addr] <= History_Table[table_addr] + 1;
+                end else
+                    History_Table[table_addr] <= History_Table[table_addr];
+            end else begin
+                if (History_Table[table_addr] > 2'b00) begin
+                    History_Table[table_addr] <= History_Table[table_addr] - 1;
+                end else begin
+                    History_Table[table_addr] <= History_Table[table_addr];
+                end
+            end
+        end else begin
+            History_Table[table_addr] <= History_Table[table_addr];
+        end
     end
 
 endmodule
--- sources/sim/Memory_Sim.cpp
+++ sources/sim/Memory_Sim.cpp
@@ -4,11 +4,11 @@
 #include <random>
 #include "VMemory.h"
 
-const char* LDST_OP = {"lb", "lh", "lw", "lbu", "lhu", "sb", "sh", "sw"};
+const char* LDST_OP[8] = {"lb", "lh", "lw", "lbu", "lhu", "sb", "sh", "sw"};
 const int SIM_TIME = 20;
 const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
 uc_engine *uc;
-vpi_handle vpi_mem[16384];
+vpiHandle vpi_mem[16384];
 
 vpiHandle get_handle(const char *name) {
     vpiHandle vh = vpi_handle_by_name((PLI_BYTE8*)name, NULL);
@@ -25,8 +25,8 @@ int get_value(vpiHandle vh) {
 	return v.value.integer;
 }
 
-int main(int argc, char** argv[]) {
-    contextp->commandArgs(argc, argv);
+int main(int argc, char* argv[]) {
+    Verilated::commandArgs(argc, argv);
 	uc_err err; int time = 0, clock = 0;
     if ((err = uc_open(UC_ARCH_RISCV, UC_MODE_32, &uc)) != UC_ERR_OK) {
         printf("Failed on uc_open() with error returned: %u\n", err);
@@ -35,8 +35,9 @@ int main(int argc, char** argv[]) {
 
 	uc_mem_map(uc, 0, 2 * 1024, UC_PROT_ALL);
 	for (int i = 0; i < 5; i++) {
+        char buffer[64];
         snprintf(buffer, 64, "TOP.Memory.test_inst.mem%d", i);
-        vpi_registers[i] = get_handle(buffer);
+        vpi_mem[i] = get_handle(buffer);
     }
 
 	while(time < SIM_TIME) {
