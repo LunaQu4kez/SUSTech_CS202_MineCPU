5a29798ebbbb74db07a98033300e781c27507209
chanbengz <chanbengz@foxmail.com>
2024-04-09 23:01:42 +0800

    test: Verilator is functioning well

--- sources/sim/Memory_Sim.cpp
+++ sources/sim/Memory_Sim.cpp
@@ -1,18 +1,17 @@
-#include <unicorn/unicorn.h>
 #include <verilated.h>
 #include <verilated_vpi.h>
+#include "verilated_vcd_c.h"
 #include <random>
 #include "VMemory.h"
 
 const char* LDST_OP[8] = {"lb", "lh", "lw", "lbu", "lhu", "sb", "sh", "sw"};
 const int SIM_TIME = 20;
 const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-uc_engine *uc;
-vpiHandle vpi_mem[16384];
+vpiHandle vpi_mem;
 
 vpiHandle get_handle(const char *name) {
     vpiHandle vh = vpi_handle_by_name((PLI_BYTE8*)name, NULL);
-    if (vh != NULL) {
+    if (vh == NULL) {
         printf("name: %s\n", name);
         vl_fatal(__FILE__, __LINE__, "get_handle", "No handle found");
     }
@@ -27,23 +26,59 @@ int get_value(vpiHandle vh) {
 
 int main(int argc, char* argv[]) {
     Verilated::commandArgs(argc, argv);
-	uc_err err; int time = 0, clock = 0;
-    if ((err = uc_open(UC_ARCH_RISCV, UC_MODE_32, &uc)) != UC_ERR_OK) {
-        printf("Failed on uc_open() with error returned: %u\n", err);
-        return -1;
-    }
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    VMemory *top = new VMemory(contextp.get());
+    top->trace(tfp, 1);
+    tfp->open("Memory_Sim.vcd");
 
-	uc_mem_map(uc, 0, 2 * 1024, UC_PROT_ALL);
-	for (int i = 0; i < 5; i++) {
-        char buffer[64];
-        snprintf(buffer, 64, "TOP.Memory.test_inst.mem%d", i);
-        vpi_mem[i] = get_handle(buffer);
-    }
+	int time = 0, mem_addr = 0;
+    top->LDST = 7, top->web = 1;
 
 	while(time < SIM_TIME) {
+        top->clka = top->clkb = 0;
+        top->addrb = mem_addr;
+        top->write_datab = 0x12345678 + time;
+        top->eval();
+        contextp->timeInc(1);
+        tfp->dump(contextp->time());
+        top->clka = top->clkb = 1;
+        top->eval();
+        contextp->timeInc(1);
+        tfp->dump(contextp->time());
+        top->clka = top->clkb = 0;
+        top->eval();
+        contextp->timeInc(1);
+        tfp->dump(contextp->time());
+        top->clka = top->clkb = 1;
+        top->eval();
+        contextp->timeInc(1);
+        tfp->dump(contextp->time());
+        top->clka = top->clkb = 0;
+        top->eval();
+        contextp->timeInc(1);
+        tfp->dump(contextp->time());
+        top->clka = top->clkb = 1;
+        top->eval();
+        contextp->timeInc(1);
+        tfp->dump(contextp->time());
+        top->clka = top->clkb = 0;
+        top->eval();
+        contextp->timeInc(1);
+        tfp->dump(contextp->time());
+        top->clka = top->clkb = 1;
+        top->eval();
+        contextp->timeInc(1);
+        tfp->dump(contextp->time());
     	VerilatedVpi::callValueCbs();
+        vpi_mem = get_handle("TOP.Memory.test_inst.mem");
+        printf("0x%x\n", get_value(vpi_handle_by_index(vpi_mem, time)));
+        mem_addr += 4;
 		time++;
 	}
-	
+
+	top->final();
+    tfp->close();
+    delete top;
 	return 0;
 }
