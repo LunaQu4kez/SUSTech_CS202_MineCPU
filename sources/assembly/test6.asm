35ee8d97b0dd98195ecf156f6d525f529bda7091
Krurmi33 <2397429787@qq.com>
2024-04-10 23:53:34 +0800

    test: jal & jalr related tests pass

---
+++ sources/assembly/test6.asm
@@ -0,0 +1,22 @@
+addi a0, zero, 8
+addi a1, zero, 3
+addi a2, zero, 5
+addi a3, zero, 1
+jal leaf
+sw a0, 4(gp)
+beq zero, zero, exit
+
+leaf:
+addi sp,sp,-8 # adjust stack for 2 items
+sw s1, 4(sp) # save s1 for use afterwards
+sw s0, 0(sp) # save s0 for use afterwards
+add s0,a0,a1 # s0 = g + h
+add s1,a2,a3 # s1 = i + j
+sub a0,s0,s1 # return value (g + h) ¨C (i + j)
+lw s0, 0(sp) # restore register s0 for caller
+lw s1, 4(sp) # restore register s1 for caller
+addi sp,sp,8 # adjust stack to delete 2 items
+jr ra # jump back to calling routine, psudo of jalr zero, 0(ra)
+
+exit:
+nop
---
+++ sources/assembly/test7.asm
@@ -0,0 +1,40 @@
+.text
+main:
+addi a0, zero, 3
+jal ra, func
+j exit
+
+func:
+addi sp, sp, -16
+sw a2, 12(sp)
+sw a1, 8(sp)
+sw ra, 4(sp)
+sw a0, 0(sp)
+
+addi t0, a0, -1
+bge zero, t0, retu
+addi a0, a0, -1
+jal ra, func
+mv a1, a0
+lw a0, 0(sp)
+addi a0, a0, -2
+jal ra, func
+mv a2, a0
+add t3, a1, a2
+lw a0, 0(sp)
+lw ra, 4(sp)
+lw a1, 8(sp)
+lw a2, 12(sp)
+addi sp, sp, 16
+mv a0, t3
+jr ra
+
+retu:
+li a0, 1
+addi sp, sp, 16
+jr ra
+
+exit:
+sw a0, 4(gp)
+
+ 
