30f6af8dfd605f46fdb0afe8b3e38c3151ca6420
Krurmi33 <2397429787@qq.com>
2024-04-18 01:16:41 +0800

    test: exception ecall pass

--- sources/sim/CPU_Sim.sv
+++ sources/sim/CPU_Sim.sv
@@ -1,18 +1,30 @@
 module CPU_Sim ();
 
     logic cpuclk, memclk, rst_n, uart_finish;
-    logic [7:0] switches;
-    logic [31:0] led;
+    logic [7:0] switches1;
+    logic [7:0] led1;
     logic [31:0] pc_t, inst_t;
-    logic [31:0] EX_data1_t, EX_data2_t, EX_imm_t, MEM_addr_t, MEM_data_t, WB_data_t, WB_mem_t, WB_data_ot;
+    logic [31:0] EX_data1_t, EX_data2_t, EX_imm_t, MEM_addr_t, MEM_data_t, WB_data_t, WB_mem_t, WB_data_ot, SEPC_t;
+    logic [7:0] switches2, switches3;
+    logic [7:0] led2, led3;
+    logic bt1, bt2, bt3, bt4, bt5;
     
     CPU cpu_inst (
         .cpuclk,
         .memclk,
         .rst_n,
         .uart_finish,
-        .switches,
-        .led_out(led),
+        .switches1,
+        .switches2,
+        .switches3,
+        .bt1,
+        .bt2,
+        .bt3,
+        .bt4,
+        .bt5,
+        .led1_out(led1),
+        .led2_out(led2),
+        .led3_out(led3),
         .pc_t,
         .inst_t,
         .EX_data1_t, 
@@ -22,7 +34,8 @@ module CPU_Sim ();
         .MEM_data_t, 
         .WB_data_t, 
         .WB_mem_t, 
-        .WB_data_ot
+        .WB_data_ot,
+        .SEPC_t
     );
 
     initial begin
@@ -30,7 +43,8 @@ module CPU_Sim ();
         cpuclk = 0;
         memclk = 1;
         uart_finish = 1;
-        switches = 0;
+        switches1 = 0;
+        bt1 = 0;
         forever begin
             #5 memclk = ~memclk;
         end
@@ -43,7 +57,13 @@ module CPU_Sim ();
     join
 
     initial fork
-        #800 $finish;
+        #200 switches1 = 8'h07;
+        #253 bt1 = 1;
+        #600 bt1 = 0;
+    join
+
+    initial fork
+        #1000 $finish;
     join
     
 endmodule
