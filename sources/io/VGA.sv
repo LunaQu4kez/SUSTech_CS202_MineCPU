a908ae6abb33ab8b78ad94ba10c9c2c5f558f939
Krurmi33 <2397429787@qq.com>
2024-04-23 23:29:49 +0800

    add: char constant

--- sources/io/VGA.sv
+++ sources/io/VGA.sv
@@ -5,9 +5,8 @@ module VGA (  // 800×600 60Hz
     // get char and color from memory
     output logic [`VGA_ADDR]  vga_addr,
     input  logic [`INFO_WID ] char,
-    input  logic [`INFO_WID ] color,    // first 4 bit means the up 8*8's color, last 4 bit means the down 8*8's color
-                                        // 0000: white   0001: yellow      0010: red        0011: pink
-                                        // 0100: orange  0101: light blue  0110: dark blue
+    input  logic [`INFO_WID ] color,    // 0: white   1: yellow      2: red        3: pink
+                                        // 4: orange  5: light blue  6: dark blue
     // output to VGA
     output logic              hsync,    // line synchronization signal
     output logic              vsync,    // vertical synchronization signal
