761a2e527e23db1531365cbc3d6847002d2cb788
Krurmi33 <2397429787@qq.com>
2024-05-17 00:14:57 +0800

    modify: modify txt output

--- tools/inst2txt.py
+++ tools/inst2txt.py
@@ -1,14 +1,24 @@
 import io
+import os
 
-filename = 'example'
+filename = 'testcase1'
 
-with io.open(filename + '.coe', 'r', encoding='utf-8') as file1:
+with io.open(filename, 'r', encoding='utf-8') as file1:
     lines = file1.readlines()
 
+if os.path.exists(filename + '.txt'):
+    os.remove(filename + '.txt')
+
 with open(filename + '.txt', 'a', encoding='utf-8') as file2:
-    for i in range(2, 16386):
+    cnt = 1
+    file2.write('00000000')
+    for i in range(len(lines)):
+        cnt += 1
         line = lines[i]
         file2.write(line[6:8])
         file2.write(line[4:6])
         file2.write(line[2:4])
         file2.write(line[0:2])
+    while cnt < 8096:
+        cnt += 1
+        file2.write('00000000')
